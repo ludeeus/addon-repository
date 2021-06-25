35b0364059a628926819d9348d1a36be0b1e9a55
Joakim Sørensen <ludeeus@ludeeus.dev>
2021-06-25 15:51:06 +0000

    Add progressbar

--- linkfiles.sh
+++ linkfiles.sh
@@ -2,5 +2,4 @@
 
 cp -f ./assets/DOCS.md "./$1/DOCS.md"
 cp -f ./assets/DOCS.md "./$1/README.md"
-cp -f ./assets/test.png "./$1/logo.png"
-cp -f ./assets/test.png "./$1/icon.png"
+cp -f ./assets/icon.png "./$1/icon.png"
